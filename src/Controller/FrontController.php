9bd2d0f97a3504896918f5722a701d6ae8c12f25
StickFunFight <rickdeurloo16@gmail.com>
2021-08-11 13:33:13 +0200

    Video 124

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -15,4 +15,50 @@ class FrontController extends AbstractController
     {
         return $this->render('front/index.html.twig');
     }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function VideoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function VideoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function SeachResults()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
