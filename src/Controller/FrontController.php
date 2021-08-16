8f967721ddaf0173c23a0a8d0069002a9f98ce2d
StickFunFight <rickdeurloo16@gmail.com>
2021-08-16 15:35:11 +0200

    t/m 148

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -20,10 +20,13 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}", name="video_list")
      */
-    public function videoList($id, CategoryTreeFrontPage $categoryTreeFrontPage)
+    public function videoList($id, CategoryTreeFrontPage $categories)
     {
-        dump($categoryTreeFrontPage);
-        return $this->render('front/video_list.html.twig');
+        $categories->getCategoryListAndParent($id);
+        dump($categories);
+        return $this->render('front/video_list.html.twig',[
+            'subcategories' => $categories
+        ]);
     }
 
     /**
